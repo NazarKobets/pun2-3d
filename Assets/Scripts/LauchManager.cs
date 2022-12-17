04eae973ff8710d2b2456708e1a0945865f388db
NazarKobets <66803921+NazarKobets@users.noreply.github.com>
2022-12-17 11:04:34 +0200

    Added code

--- Assets/Scripts/LauchManager.cs
+++ Assets/Scripts/LauchManager.cs
@@ -5,17 +5,54 @@ using UnityEngine;
 
 public class LauchManager : MonoBehaviourPunCallbacks
 {
+    public GameObject EnterGamePanel;
+    public GameObject ConnectionStatusPanel;
+    public GameObject LobbyPanel;
 
+
+    #region Unity Methods
     void Start()
     {
-        PhotonNetwork.ConnectUsingSettings();
+        EnterGamePanel.SetActive(true);
+        ConnectionStatusPanel.SetActive(false);
+        LobbyPanel.SetActive(false);
+        //PhotonNetwork.ConnectUsingSettings();
     }
 
 
     void Update()
     {
-        
+
     }
+    #endregion
+
+
+    #region Photon Callbacks
+    public void ConnectToPhotonServer()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            EnterGamePanel.SetActive(false);
+            ConnectionStatusPanel.SetActive(true);
+        }
+    }
+    #endregion
+
+
+    #region Public Methods
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(message: PhotonNetwork.NickName + " Connected to server");
+        ConnectionStatusPanel.SetActive(false);
+        LobbyPanel.SetActive(true);
+    }
+
+    public override void OnConnected()
+    {
+        Debug.Log(message: "Connected to internet");
+    }
+    #endregion
 
 
 }
