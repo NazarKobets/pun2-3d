88572dfafcd88dfe3ec89096949eb7218a4662d3
NazarKobets <66803921+NazarKobets@users.noreply.github.com>
2022-12-17 18:04:44 +0200

    Added new scene

--- Assets/Scripts/LauchManager.cs
+++ Assets/Scripts/LauchManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 public class LauchManager : MonoBehaviourPunCallbacks
@@ -24,21 +25,8 @@ public class LauchManager : MonoBehaviourPunCallbacks
     {
 
     }
+
     #endregion
-
-
-    #region Photon Callbacks
-    public void ConnectToPhotonServer()
-    {
-        if (!PhotonNetwork.IsConnected)
-        {
-            PhotonNetwork.ConnectUsingSettings();
-            EnterGamePanel.SetActive(false);
-            ConnectionStatusPanel.SetActive(true);
-        }
-    }
-    #endregion
-
 
     #region Public Methods
     public override void OnConnectedToMaster()
@@ -52,7 +40,60 @@ public class LauchManager : MonoBehaviourPunCallbacks
     {
         Debug.Log(message: "Connected to internet");
     }
+
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
     #endregion
+
+    #region Photon Callbacks
+
+    public void ConnectToPhotonServer()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            EnterGamePanel.SetActive(false);
+            ConnectionStatusPanel.SetActive(true);
+        }
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        Debug.Log(message);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(message: PhotonNetwork.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(message: newPlayer.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name + " player in room " + PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void CreateAndJoinRoom()
+    {
+        string randomRoomName = "Room" + Random.Range(0, 10000);
+        RoomOptions roomOptions = new RoomOptions
+        {
+            MaxPlayers = 20,
+            IsOpen = true,
+            IsVisible = true
+        };
+
+        PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
+    }
+
+    #endregion
+
 
 
 }
