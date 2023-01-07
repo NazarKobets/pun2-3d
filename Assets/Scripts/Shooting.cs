da7c83ca398f3faaf6890150b767eb4f39b6360a
NazarKobets <66803921+NazarKobets@users.noreply.github.com>
2023-01-07 11:15:50 +0200

    Update Shooting.cs

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -15,5 +15,18 @@ public class Shooting : MonoBehaviour
         {
             fireTimer += Time.deltaTime;
         }
+
+        if (Input.GetButton("Fire1") && fireTimer > fireRate)
+        {
+            fireTimer = 0;
+
+            // Shooting
+            Ray ray = fpsCamera.ViewportPointToRay(pos: new Vector3(x: 0.5f, y: 0.5f));
+
+            if (Physics.Raycast(ray, out var hit, maxDistance:100))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
+        }
     }
 }
